448c49dadd86751241792d293f4dd744c3f1f37e
Yuu <fremysd@yandex.com>
2016-12-25 12:06:58 +0200

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Yuu's Vayne 2017")]
+[assembly: AssemblyDescription("EloBuddy 2017 Vayne Addon")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yuu")]
+[assembly: AssemblyCopyright("Yuu © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bdee56a-4ceb-43b4-b192-59385ebcb1c4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
